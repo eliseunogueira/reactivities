9c1532e425ce96285a44dc4efcb3665a86bc201d
eliseunogueira <eliseu.nogueira@outlook.com>
2020-03-30 16:18:53 -0300

    added semantic ui react

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
 
 class App extends Component {
   state = {
@@ -17,15 +18,16 @@ class App extends Component {
   }
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
